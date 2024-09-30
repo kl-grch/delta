66a5f6746c5268014a5e0ca61a699c33932c6a6f
Nikolay Gerasimov <kolyagerich@gmail.com>
2024-09-30 18:04:01 +0300

    test work

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,198 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { Fragment, useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Chart from "@/components/chart/Chart";
+
+interface DataRow {
+  id: number;
+  indicator: string;
+  today: number;
+  yesterday: number;
+  thisWeekday: number;
+}
+
+const data: DataRow[] = [
+  {
+    id: 1,
+    indicator: "Выручка, руб",
+    today: 500521,
+    yesterday: 480521,
+    thisWeekday: 4805121,
+  },
+  {
+    id: 2,
+    indicator: "Наличные",
+    today: 300000,
+    yesterday: 300000,
+    thisWeekday: 300000,
+  },
+  {
+    id: 3,
+    indicator: "Безналичный расчет",
+    today: 100000,
+    yesterday: 100000,
+    thisWeekday: 100000,
+  },
+  {
+    id: 4,
+    indicator: "Кредитные карты",
+    today: 100521,
+    yesterday: 100521,
+    thisWeekday: 100521,
+  },
+  {
+    id: 5,
+    indicator: "Средний чек, руб",
+    today: 1300,
+    yesterday: 900,
+    thisWeekday: 900,
+  },
+  {
+    id: 6,
+    indicator: "Средний гость, руб",
+    today: 1200,
+    yesterday: 800,
+    thisWeekday: 800,
+  },
+  {
+    id: 7,
+    indicator: "Удаление из чека (после оплаты), руб",
+    today: 1000,
+    yesterday: 1100,
+    thisWeekday: 900,
+  },
+  {
+    id: 8,
+    indicator: "Удаление из чека (до оплаты), руб",
+    today: 1300,
+    yesterday: 1300,
+    thisWeekday: 900,
+  },
+  {
+    id: 9,
+    indicator: "Количество чеков",
+    today: 34,
+    yesterday: 36,
+    thisWeekday: 34,
+  },
+  {
+    id: 10,
+    indicator: "Количество гостей",
+    today: 34,
+    yesterday: 36,
+    thisWeekday: 32,
+  },
+];
+
+export default function Component() {
+  const [expandedRow, setExpandedRow] = useState<number | null>(null);
+
+  const toggleRow = (id: number) => {
+    setExpandedRow(expandedRow === id ? null : id);
+  };
+
+  function getProcentage(today: number, yesterday: number): number {
+    const result = Number(((today / yesterday) * 100 - 100).toFixed(0));
+    return result;
+  }
+
+  function getColorBgYesterday(yesterday: number, today: number) {
+    let color;
+    if (yesterday < today) {
+      color = "bg-green-200";
+    } else if (yesterday > today) {
+      color = "bg-red-200";
+    } else {
+      color = "bg-gray-100";
+    }
+    return color;
+  }
+
+  function getColorWeek(week: number, today: number) {
+    let color;
+    if (week > today) {
+      color = "bg-red-200";
+    } else if (week == today) {
+      color = "bg-gray-100";
+    } else {
+      color = "bg-green-200";
+    }
+    return color;
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Table className="border-separate border-spacing-2 border border-slate-400">
+      <TableHeader>
+        <TableRow>
+          <TableHead className="bg-gray-100 text-center">Показатель</TableHead>
+          <TableHead className="bg-blue-100 text-center">
+            Текущий день
+          </TableHead>
+          <TableHead className="bg-gray-100 text-center">Вчера</TableHead>
+          <TableHead className="bg-gray-100 text-center">
+            Этот день недели
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {data.map((row) => (
+          <Fragment key={row.id}>
+            <TableRow
+              // key={row.id}
+              className="cursor-pointer"
+              onClick={() => toggleRow(row.id)}
+            >
+              <TableCell className="bg-gray-100">{row.indicator}</TableCell>
+              <TableCell className="bg-blue-100 text-end">
+                {row.today}
+              </TableCell>
+              <TableCell
+                className={
+                  getColorBgYesterday(row.yesterday, row.today) +
+                  " text-end flex justify-between"
+                }
+              >
+                <div className="">{row.yesterday}</div>
+                <div
+                  className={
+                    (getProcentage(row.today, row.yesterday) < 0
+                      ? "text-red-600"
+                      : "text-green-600") + " font-bold"
+                  }
+                >
+                  {getProcentage(row.today, row.yesterday)}%
+                </div>
+              </TableCell>
+              <TableCell
+                className={
+                  getColorWeek(row.thisWeekday, row.today) + "  text-end"
+                }
+              >
+                {row.thisWeekday}
+              </TableCell>
+            </TableRow>
+            {expandedRow === row.id && (
+              <TableRow>
+                <TableCell colSpan={5}>
+                  <div className="p-4 bg-white">
+                    <div className=" flex items-center justify-center">
+                      <Chart />
+                    </div>
+                  </div>
+                </TableCell>
+              </TableRow>
+            )}
+          </Fragment>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
